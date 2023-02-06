25c35210d97e8d10704a66721c4f66306c59825f
孙斌 <sunbin@xinyu668.com>
2023-02-06 16:25:11 +0800

    内存分析

---
+++ profiling/profiling_memory.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%%% @author sunbin
+%%% @copyright (C) 2023, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 06. 2月 2023 16:23
+%%%-------------------------------------------------------------------
+-module(profiling_memory).
+-author("sunbin").
+
+%% API
+-export([memory/0]).
+
+memory() ->
+  memory(10).
+memory(N) ->
+  [erlang:process_info(P)||{P, _}<-lists:sublist(lists:sort(fun({_, {_, A}}, {_, {_, B}}) -> A > B end, [{P,erlang:process_info(P,memory)} || P <- erlang:processes()]), N)].
