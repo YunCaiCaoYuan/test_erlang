26f0ca4970b87ee6aed06549bb956e480a0a16f4
孙斌 <sunbin@xinyu668.com>
2023-04-06 17:03:04 +0800

    分号语法

---
+++ grammar/test_grammar.erl
@@ -0,0 +1,23 @@
+%%%-------------------------------------------------------------------
+%%% @author sunbin
+%%% @copyright (C) 2023, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 06. 4月 2023 16:53
+%%%-------------------------------------------------------------------
+-module(test_grammar).
+-author("sunbin").
+
+%% API
+-export([test_semicolon/1]).
+
+test_semicolon(Val) ->
+	if
+		Val == 1; Val == 2 ->
+			io:format("1 or 2~n");
+		Val == 3; Val == 4 ->
+			io:format("3 or 4~n");
+		true ->
+			io:format("other~n")
+	end.
